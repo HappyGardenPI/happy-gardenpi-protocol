c662c5aff74b7878391eaad8a313e9505de32ac0
Antonio Salsi <passy.linux@zresa.it>
2021-09-12 23:27:35 +0200

    Update protocol.hpp

--- include/hgardenpi-protocol/protocol.hpp
+++ include/hgardenpi-protocol/protocol.hpp
@@ -66,7 +66,7 @@ namespace hgardenpi::protocol
          * @param buffer will be modified
          * @param id id tu assign
          */
-        [[maybe_unused]]  void updateIdToBufferEncoded(Buffer &buffer, uint8_t id);
+        [[maybe_unused]] void updateIdToBufferEncoded(Buffer &buffer, uint8_t id);
 
     }
 }
